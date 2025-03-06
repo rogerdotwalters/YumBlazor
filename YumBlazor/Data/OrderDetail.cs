31b7f616ea7b9029415c88c63ae4cb1303369e9f
rogerdotwalters <rogerwalters3@gmail.com>
2025-03-06 11:09:59 -0600

    Last Commit

---
+++ YumBlazor/Data/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data {
+	public class OrderDetail {
+
+		public int Id { get; set; }
+		public int OrderHeaderId { get; set; }
+		public int ProductId { get; set; }
+		public Product Product { get; set; }
+		[Required]
+		public int Count { get; set; }
+		[Required]
+		public double Price { get; set; }
+		[Required]
+		public string? ProductName { get; set; }
+	}
+}
